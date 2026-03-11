ab5ff12939929250f4576f2e03f7d04baa42fa75
TimeEternal <timeeternal@example.com>
2026-03-11 19:52:13 +0800

    Update blog author name and bio

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "时光静逸",
+	bio: "凡心所向，素履以往；生如逆旅，一苇以航。",
 	links: [
 		{
 			name: "Twitter",
